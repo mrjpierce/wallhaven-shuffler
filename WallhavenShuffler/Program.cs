83b0b70bc172e6d28906b450f6dddc4546a4c205
James Pierce <mr.jpierce@gmail.com>
2016-08-15 16:20:51 -0600

    Project Setup

---
+++ WallhavenShuffler/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WallhavenShuffler
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Settings());
+        }
+    }
+}
